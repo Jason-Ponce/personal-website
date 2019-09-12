bde214dfae794a4a70a81bcd4abf75f9573f0e6b
Unknown <jason.ponce.work@gmail.com>
2019-09-12 13:42:37 -0400

    Initial Upload of project so far

---
+++ jasonsite/routes.py
@@ -0,0 +1,22 @@
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request, abort
+from jasonsite import app, db, bcrypt
+# from jasonsite.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+# from jasonsite.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+
---
+++ run.py
@@ -0,0 +1,4 @@
+from jasonsite import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
