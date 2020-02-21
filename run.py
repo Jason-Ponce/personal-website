923d15ebf1d213bbdff9b8990f892719223106ff
JP <jasonponcework@gmail.com>
2020-02-21 14:08:55 -0500

    production update
    -Changed env to prod
    -set ip for prod

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from jasonsite import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80, debug=False)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
