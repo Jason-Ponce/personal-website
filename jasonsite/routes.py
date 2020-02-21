f10d07bc155efd5962d88765304f1a2a9ff5eaa6
JP <jasonponcework@gmail.com>
2020-02-21 14:04:09 -0500

    Changing site map of website
    -removed links and routes for art class homework
    -simplifying routes and links
    -updating css for home,projects
    
    |-----TO DO-----|
    -Redo about page
    -finish up projects page
    -spice up resume page
    
    |-----NEXT STEPS----|
    -Add Database (Important)

--- jasonsite/routes.py
+++ jasonsite/routes.py
@@ -32,78 +32,19 @@ def projects():
     return render_template('projects.html', title=page_title)
 
 # |--GRAPHIC DESIGN SECTION--|
-@app.route("/projects/graphic_design")
+@app.route("/graphic_design")
 def graphic_design():
     page_title="Graphic Design"
     return render_template('graphic_design.html', title=page_title)
 
 
-@app.route("/projects/graphic_design/case_studies")
-def case_studies():
-    page_title="Case Studies"
-    return render_template('case_studies.html', title=page_title)
-
-
-@app.route("/projects/graphic_design/case_studies/form_storming")
-def form_storming():
-    page_title="Form Storming"
-    return render_template('form_storming.html', title=page_title)
-
-
-@app.route("/projects/graphic_design/case_studies/monogram")
-def monogram():
-    page_title="Monogram"
-    return render_template('monogram.html', title=page_title)
-
-
-@app.route("/projects/graphic_design/case_studies/logo")
-def logo():
-    page_title="Logo"
-    return render_template('logo.html', title=page_title)
-
-
-@app.route("/projects/graphic_design/case_studies/historical_figure")
-def historical_fig():
-    page_title="Historical Figure"
-    return render_template('historical_figure.html', title=page_title)
-
-
-@app.route("/projects/graphic_design/case_studies/travel_poster")
-def travel_poster():
-    page_title="Travel Poster"
-    return render_template('travel_poster.html', title=page_title)
-
-
-@app.route("/projects/graphic_design/case_studies/portfolio")
+@app.route("/projects/portfolio")
 def portfolio():
     page_title="Portfolio"
-    return render_template('portfolio.html', title=page_title)
-
-@app.route("/projects/graphic_design/photoshop_experiments")
-def photoshop_experiments():
-    page_title="Photoshop Experiments"
-    return render_template('photoshop_experiments.html', title=page_title)    
-
-
-@app.route("/projects/graphic_design/photoshop_experiments/EXPLOSION")
-def EXPLOSION():
-    page_title="EXPLOSION"
-    return render_template('EXPLOSION.html', title=page_title)    
-
-
-@app.route("/projects/graphic_design/photoshop_experiments/underwater_effect")
-def underwater_effect():
-    page_title="Underwater Effect"
-    return render_template('underwater_effect.html', title=page_title)    
-
-
-@app.route("/projects/graphic_design/photoshop_experiments/smudge_script")
-def smudge_script():
-    page_title="Smudge Script"
-    return render_template('smudge_script.html', title=page_title)    
-
-    
+    return render_template('portfolio.html', title=page_title)   
 # END GRAPHIC DESIGN SECTION
+
+
 # |--WEB DESIGN SECTION--|
 @app.route("/projects/web_design")
 def web_design():
@@ -117,6 +58,12 @@ def web_design():
 def web_development():
     page_title="Web Development"
     return render_template('web_development.html', title=page_title)
+
+
+@app.route("/web_development/jasonponce_info")
+def jasonponce():
+    page_title="JasonPonce.Info"
+    return render_template('jasonponce_info.html', title=page_title)
 
 
 # END WEB DEVELOPMENT SECTION
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from jasonsite import app
 
 if __name__ == '__main__':
-    # app.run(debug=False)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=80, debug=False)
