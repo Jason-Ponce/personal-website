f39eeb19a5039cfbed7fa21013da8003827b6cd8
JP <jasonponcework@gmail.com>
2020-03-05 21:16:31 -0500

    Rework of graphic design page
    -deleted old images that arent used
    -fixed up the gallery section of  graphic design

--- jasonsite/static/js/app.js
+++ jasonsite/static/js/app.js
@@ -1,30 +1,41 @@
-// $(document).ready(() => {
-//     $('.slideshow .slick').slick({
-//       autoplay: true,
-//       fade: true,
-//       autoplaySpeed: 2000,
-//       speed: 4000,
-//       dots: true,
-//       swipe: true
-//     });
-//   });
+// MaterializeCSS scrollspy
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
 
-
+// Plugin slick slideshow  
 $(document).ready(() => {
-    $('.slick').slick({
-        fade: true,
-        adaptiveHeight: true,
-        // centerMode: true,
-        prevArrow: '<i class="material-icons md-48 md-dark slick-prev" alt="Slide image to left">keyboard_arrow_left</i>',
-        nextArrow: '<i class="material-icons md-48 md-dark slick-next" alt="Slide image to right">keyboard_arrow_right</i>',
-    });
+  $('.slick').slick({
+      fade: true,
+      adaptiveHeight: true,
+      prevArrow: '<i class="material-icons md-48 md-dark slick-prev" alt="Slide image to left">keyboard_arrow_left</i>',
+      nextArrow: '<i class="material-icons md-48 md-dark slick-next" alt="Slide image to right">keyboard_arrow_right</i>',
+  });
 
-    $(".lazy").slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true
-      });
-    });
+  $('.slick-portfolio-travel').slick({
+    fade: true,
+    // adaptiveHeight: true,
+    draggable: false,
+    prevArrow: '<i class="material-icons md-48 md-dark slick-prev" onclick="changeTravelPrev()" alt="Slide image to left">keyboard_arrow_left</i>',
+    nextArrow: '<i class="material-icons md-48 md-dark slick-next" onclick="changeTravelNext()" alt="Slide image to right">keyboard_arrow_right</i>',
+  });
+
+  $('.slick-portfolio-stationary').slick({
+    fade: true,
+    // adaptiveHeight: true,
+    draggable: false,
+    prevArrow: '<i class="material-icons md-48 md-dark slick-prev" onclick="changeStationaryPrev()" alt="Slide image to left">keyboard_arrow_left</i>',
+    nextArrow: '<i class="material-icons md-48 md-dark slick-next" onclick="changeStationaryNext()" alt="Slide image to right">keyboard_arrow_right</i>',
+  });
+
+  $('.slick-portfolio-logo').slick({
+    fade: true,
+    // adaptiveHeight: true,
+    draggable: false,
+    prevArrow: '<i class="material-icons md-48 md-dark slick-prev" onclick="changeLogoPrev()" alt="Slide image to left">keyboard_arrow_left</i>',
+    nextArrow: '<i class="material-icons md-48 md-dark slick-next" onclick="changeLogoNext()" alt="Slide image to right">keyboard_arrow_right</i>',
+  });
+});
 
 
   
-//   console.log("hi jason")
