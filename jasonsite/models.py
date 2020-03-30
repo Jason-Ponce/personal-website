04fa582703e8fd79602a362c682324edd258c005
JP <jasonponcework@gmail.com>
2020-03-30 00:59:31 -0400

    changed styling, parameters and return for object

--- jasonsite/models.py
+++ jasonsite/models.py
@@ -13,29 +13,29 @@ class User(db.Model, UserMixin):
     password =      db.Column(db.String(60), nullable=False)
     first_name =    db.Column(db.String(64), nullable=False)
     last_name =     db.Column(db.String(64), nullable=False)
-    referral_code = db.Column(db.String(16), unique=True, nullable=False)
+    active =        db.Column(db.Boolean, nullable=False)
     posts =         db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.email}', '{self.image_file}', '{self.first_name}', '{self.last_name}')"
+        return f"User('{self.email}', '{self.active}', '{self.first_name}', '{self.last_name}')"
 
 
 class Post(db.Model, UserMixin):
-    post_id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    title = db.Column(db.String(64), nullable = False)
-    date_posted = db.Column(db.String, nullable = False)
-    blurb = db.Column(db.String(256), nullable = False)
-    content = db.Column(db.Text, nullable = False)
-    pill_images = db.relationship('Images', backref = 'post_images', lazy = True)
+    post_id =       db.Column(db.Integer, primary_key = True)
+    user_id =       db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    title =         db.Column(db.String(64), nullable = False)
+    date_posted =   db.Column(db.String, nullable = False)
+    blurb =         db.Column(db.String(256), nullable = False)
+    content =       db.Column(db.Text, nullable = False)
+    pill_images =   db.relationship('Images', backref = 'post_images', lazy = True)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}', '{self.blurb}', '{self.content}', '{self.pill_images}')"
 
 class Images(db.Model, UserMixin):
-    image_id = db.Column(db.Integer, primary_key = True)
-    source = db.Column(db.Text, nullable = False)
-    posting_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable = False)
+    image_id =      db.Column(db.Integer, primary_key = True)
+    source =        db.Column(db.Text, nullable = False)
+    posting_id =    db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable = False)
 
     def __repr__(self):
         return f"Images('{self.source}')"
