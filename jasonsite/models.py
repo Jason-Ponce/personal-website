f775a791e7b9de76cecd4c68d7530d114910ab17
JP <jasonponcework@gmail.com>
2020-04-07 21:40:11 -0400

    formatted styling

--- jasonsite/models.py
+++ jasonsite/models.py
@@ -7,14 +7,14 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    id =            db.Column(db.Integer, primary_key=True)
-    email =         db.Column(db.String(120), unique=True, nullable=False)
-    profile_image = db.Column(db.String(20), nullable=False, default='default.png')
-    password =      db.Column(db.String(60), nullable=False)
-    first_name =    db.Column(db.String(64), nullable=False)
-    last_name =     db.Column(db.String(64), nullable=False)
-    status =        db.Column(db.Boolean, nullable=False)
-    posts =         db.relationship('Post', backref='author', lazy=True)
+    id              = db.Column(db.Integer, primary_key=True)
+    email           = db.Column(db.String(120), unique=True, nullable=False)
+    profile_image   = db.Column(db.String(20), nullable=False, default='default.png')
+    password        = db.Column(db.String(60), nullable=False)
+    first_name      = db.Column(db.String(64), nullable=False)
+    last_name       = db.Column(db.String(64), nullable=False)
+    status          = db.Column(db.Boolean, nullable=False)
+    posts           = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.email}', '{self.status}', '{self.first_name}', '{self.last_name}')"
@@ -24,9 +24,9 @@ class Post(db.Model, UserMixin):
     post_id         = db.Column(db.Integer, primary_key = True)
     user_id         = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
     title           = db.Column(db.String(64), nullable = False)
-    date_posted     = db.Column(db.String, nullable = False)
+    date_posted     = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
     blurb           = db.Column(db.String(256), nullable = False)
-    content         = db.Column(db.Text, nullable = False)
+    post            = db.Column(db.Text, nullable = False)
     pill_images     = db.relationship('Images', backref = 'post_images', lazy = True)
     category        = db.Column(db.String(32), nullable = False)
     created_by      = db.relationship('User', backref = 'owner', lazy=True)
@@ -35,9 +35,9 @@ class Post(db.Model, UserMixin):
         return f"Post('{self.title}', '{self.blurb}', '{self.date_posted}', '{self.content}', '{self.category }')"
 
 class Images(db.Model, UserMixin):
-    image_id =      db.Column(db.Integer, primary_key = True)
-    source =        db.Column(db.Text, nullable = False)
-    posting_id =    db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable = False)
+    image_id        = db.Column(db.Integer, primary_key = True)
+    source          = db.Column(db.Text, nullable = False)
+    posting_id      = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable = False)
 
     def __repr__(self):
         return f"Images('{self.source}')"
