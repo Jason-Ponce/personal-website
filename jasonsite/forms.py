606633a734cdc255a444e6dd7be66f334e7c85ca
JP <jasonponcework@gmail.com>
2020-09-27 17:56:04 -0400

    Adding tag posting inputs

--- jasonsite/forms.py
+++ jasonsite/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField, SelectField, FormField, Form
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from jasonsite.models import User
 
@@ -30,17 +30,30 @@ class AdminToolForm(FlaskForm):
     user            = StringField("Email", validators=[DataRequired("Please Input Email")])
     submit          = SubmitField('Submit')
 
+class ExtendedPost(Form):
+    post            = TextAreaField('Post Text')
+    fig_caption     = StringField('Fig caption...') 
+    alt_title       = StringField("Accessibility description")
+    post_image      = FileField('File', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'pdf'])])
+    submit          = SubmitField('Submit')
+
+
 class PostForm(FlaskForm):
     title           = StringField('Title')
     post            = TextAreaField('Post Text')
     blurb           = StringField('Blurb')
     category_post   = SelectField('Category', choices=[('web_development','Web Development'), ('web_design','Web Design'), ('graphic_design','Graphic Design') ])
     post_image      = FileField('File', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'pdf'])])
-    alt_title       = StringField("Alt description", validators=[DataRequired(message="Remember accessibility!")])
+    alt_title       = StringField("Accessibility description", validators=[DataRequired(message="For accessibility")])
     tag1            = StringField('Tag one...')
     tag2            = StringField('Tag two...')
     tag3            = StringField('Tag three...')
     tag4            = StringField('Tag four...')
     tag5            = StringField('Tag five...')
     fig_caption     = StringField('Fig caption...')
+    ext_post1       = FormField(ExtendedPost)
+    ext_post2       = FormField(ExtendedPost)
+    ext_post3       = FormField(ExtendedPost)
+    ext_post4       = FormField(ExtendedPost)
+    ext_post5       = FormField(ExtendedPost)
     submit          = SubmitField('Submit')
